cc0432bab4043953915590d6ac4c2e53999e1464
Thago MadPin <madpin@gmail.com>
2022-08-21 03:53:19 +0000

    update 2

--- docker/website/dump_workpress.sh
+++ docker/website/dump_workpress.sh
@@ -1,1 +1,1 @@
-docker exec wordpress_db sh -c 'exec mysqldump --all-databases -uexampleuser -p"$examplepass"' > ~/configs/website/bkp/wordpress_db.sql
+docker exec wordpress_db sh -c 'exec mysqldump --all-databases -u exampleuser -pexamplepass' > ~/configs/website/bkp/wordpress_db.sql
